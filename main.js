3c6c3e372b4c7af27e1d2549d55f1a92936ec26e
byronleetinker <byronleetinker03@gmail.com>
2021-08-12 16:36:21 +0200

    update

---
+++ main.js
@@ -0,0 +1,27 @@
+let signInButton = document.querySelector(".but1");
+
+function login(username, password) {
+  console.log(username);
+  console.log(password);
+  fetch("", {
+    method: "POST",
+    body: JSON.stringify({
+      username: `${username}`,
+      password: `${password}`,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data["access_token"]) {
+        console.log(data);
+        myStorage = window.localStorage;
+        myStorage.setItem("jwt-token", data["access_token"]);
+        myStorage.setItem("username", username);
+        myStorage.setItem("password", password);
+        window.location.href = "/products.html";
+      }
+    });
+}
